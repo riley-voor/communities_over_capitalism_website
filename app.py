87163c85cf80810475e0fdccd5443b28e181db92
Riley Voor <riley.voor@gmail.com>
2021-01-08 21:19:05 -0500

    Moved the page_html.html render functionality into its own function to keep it clean (also did some other minor cleaning)

--- app.py
+++ app.py
@@ -10,16 +10,28 @@ app = Flask(__name__)
 ### Routes ###
 ##############
 
-# NOTE: We always render the page_base.html template and then pass in the page we want to 
-# render. This is cleaner and keeps repeated code out of the individual page templates.
-
 @app.route('/')
 def landing_page():
-    return render_template( 'page_base.html', page='landing_page.html' );
+    return render_page( page='landing_page.html' )
 
 @app.route('/about_us')
 def about_us():
-    return render_template( 'page_base.html', page='about_us.html', title='About Us' );
+    return render_page( page='about_us.html', title='About Us' )
+
+######################
+### Util Functions ###
+######################
+
+# We always render the page_base.html template and then pass in the page we want to 
+# render. This is cleaner and keeps repeated code out of the individual page templates.
+def render_page( **args ):
+
+    # Check that we have our required arguments
+    if( 'page' not in args):
+        raise Exception( 'Page was not passed to render_page function' )
+
+    # Pass those params to the render_template function along with our page_base.html template.
+    return render_template( 'page_base.html', **args )
 
 if __name__ == '__main__':
     app.run()
