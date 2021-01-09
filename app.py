ac4d8239762970074db4fe392dd0d7f0ddbc34c3
Riley Voor <riley.voor@gmail.com>
2021-01-08 20:54:31 -0500

    moved nav bar to a partial and page header and title to a page base wrapper

--- app.py
+++ app.py
@@ -10,9 +10,16 @@ app = Flask(__name__)
 ### Routes ###
 ##############
 
+# NOTE: We always render the page_base.html template and then pass in the page we want to 
+# render. This is cleaner and keeps repeated code out of the individual page templates.
+
 @app.route('/')
 def index():
-    return render_template( 'index.html' );
+    return render_template( 'page_base.html', page='index.html' );
+
+@app.route('/about_us')
+def about_us():
+    return render_template( 'page_base.html', page='about_us.html', title='About Us' );
 
 if __name__ == '__main__':
     app.run()
