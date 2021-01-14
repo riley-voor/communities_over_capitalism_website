362d16d0e04b82b0649028c3de40601c8b15d010
Riley Voor <riley.voor@gmail.com>
2021-01-13 22:02:00 -0500

    setup blog posts json file so it isn't tracked by git and will be generated when the server starts up

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,13 @@
+# IF the blog posts json file doesnt yet exist then generate it and make it an empty array
+# However if it already exists then don't touch it
+if ! test -f blog_posts.json; then
+    echo "Generating blog posts json file..."
+    touch blog_posts.json
+    echo "[]" >> blog_posts.json
+    echo "Blog posts json file generated!"
+else
+    echo "Skipping blog posts json file generation..."
+fi
+
+echo "Starting flask server..."
 FLASK_APP=app.py FLASK_ENV=development flask run
